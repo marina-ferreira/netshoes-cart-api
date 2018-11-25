7407d3d6664372f84474ee9f14743f97590f86ea
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-11-25 14:02:19 -0200

    Server products endpoint

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const cors = require('cors');
 
+app.use(cors());
+
+app.get('/products', (req, res) => {
+  let repo = 'https://raw.githubusercontent.com/freedom-netshoes/front-end-recruitment/';
+  let file = 'master/public/data/products.json';
+  let url = repo + file;
+
+  axios.get(url).then(response => res.send(response.data));
+});
+
+app.listen(3000, function() {
+  console.log('listening on 3000');
+});
